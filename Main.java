49c848383f451aabe55f6e75aff3a43ea4f6c282
MichaelJahns <jahnsmic001@gmail.com>
2019-04-16 08:16:45 -0700

    Adds coinFlip functionality.

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main{
       pluraize("Lizard", 0);
       pluraize("Pig", 1);
       pluraize("Pigmy Elephant", 17);
+
+      flipNHeads(4);
     }
     public static void pluraize(String animal, int herdSize){
       if(herdSize == 0){
@@ -12,6 +14,24 @@ public class Main{
         System.out.println("You have a " + animal + "?");
       } else{
         System.out.println("You have " + herdSize + " " +animal + "'s?");
+      }
+    }
+    public static void flipNHeads(int n){
+      int heads = 0;
+      int attempts = 0;
+ 
+      while(heads < n){
+        attempts ++;
+        int flip = (int) Math.round(Math.random());
+        if(flip == 0){
+          System.out.println("tails");
+        }if(flip == 1){
+          System.out.println("heads");
+          heads++;
+        }
+      }
+      if(heads == n){
+        System.out.println("It took " + attempts + " coin flips to get " + n + " heads");
       }
     }
 }
